d852654b658059bd3eed2a3ab3f2d30e55106f51
Tavantai1234 <155410449+tavantai1234@users.noreply.github.com>
2026-02-27 00:44:56 +0700

    Fix rampup 1000  and consistency 0.3

--- code/train_uncertainty_rectified_pyramid_consistency_2D.py
+++ code/train_uncertainty_rectified_pyramid_consistency_2D.py
@@ -54,9 +54,9 @@ parser.add_argument('--labeled_num', type=int, default=7,
                     help='labeled data')
 # costs
 parser.add_argument('--consistency', type=float,
-                    default=0.1, help='consistency')
+                    default=0.3, help='consistency')
 parser.add_argument('--consistency_rampup', type=float,
-                    default=200.0, help='consistency_rampup')
+                    default=1000, help='consistency_rampup')
 args = parser.parse_args()
 
 
